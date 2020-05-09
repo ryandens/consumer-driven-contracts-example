da3c484b1b7d01893e473ecfc3b21246fdab0e7e
Ryan Dens <ryan.dens@contrastsecurity.com>
2020-05-09 15:37:01 -0400

    :sparkles: implement client code

--- consumer/src/main/java/com/github/ryandens/consumer/Main.java
+++ consumer/src/main/java/com/github/ryandens/consumer/Main.java
@@ -1,11 +1,45 @@
 package com.github.ryandens.consumer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.ryandens.consumer.messages.CoffeeOrder;
+import com.github.ryandens.consumer.messages.Receipt;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public final class Main {
+
+  private static final HttpClient httpClient = HttpClient.newHttpClient();
+  private static final ObjectMapper objectMapper = new ObjectMapper();
 
   /**
    * Entry point of the API Consumer application, responsible for starting and stopping resources
    */
   public static void main(final String[] args) {
-    System.out.println("hello world");
+    final Receipt receipt;
+    try {
+      receipt =
+          sendOrder(
+              CoffeeOrder.of(CoffeeOrder.Size.LARGE, CoffeeOrder.Bean.CATURRA),
+              "http://ryandens.com");
+    } catch (InterruptedException | IOException e) {
+      throw new RuntimeException(e);
+    }
+    System.out.println("receipt: " + receipt.toString());
+  }
+
+  public static Receipt sendOrder(final CoffeeOrder coffeeOrder, final String hostName)
+      throws InterruptedException, IOException {
+    final var request =
+        HttpRequest.newBuilder(URI.create(hostName + "/coffee"))
+            .header("Content-Type", "application/json")
+            .POST(
+                HttpRequest.BodyPublishers.ofByteArray(objectMapper.writeValueAsBytes(coffeeOrder)))
+            .build();
+
+    return objectMapper.readValue(
+        httpClient.send(request, HttpResponse.BodyHandlers.ofString()).body(), Receipt.class);
   }
 }
