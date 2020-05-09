ae05573af31253c73f32e93e455dca38270e1847
Ryan Dens <ryan.dens@contrastsecurity.com>
2020-05-09 15:36:42 -0400

    :heavy_plus_sign: add dependencies for autovalue and jackson

--- consumer/build.gradle.kts
+++ consumer/build.gradle.kts
@@ -5,3 +5,12 @@ plugins {
 application {
     mainClassName = "com.github.ryandens.consumer.Main"
 }
+
+dependencies {
+    // AutoValue for reducing boilerplate in creating immutable value objects
+    val autoValueVersion = "1.7"
+    compileOnly("com.google.auto.value", "auto-value-annotations", autoValueVersion)
+    annotationProcessor("com.google.auto.value", "auto-value", autoValueVersion)
+    // jackson for object serialization/deserialization
+    implementation("com.fasterxml.jackson.core", "jackson-databind", "2.11.0")
+}
