ad07c8dbb32f30e57b1cfce1ff93ec0c55db44b4
Ryan Dens <ryan.dens@contrastsecurity.com>
2020-05-09 13:01:54 -0400

    :heavy_plus_sign: add dependencies for jersey and jax-rs and autovalue and jackson

--- provider/build.gradle.kts
+++ provider/build.gradle.kts
@@ -5,3 +5,20 @@ plugins {
 application {
     mainClassName = "com.github.ryandens.provider.Main"
 }
+
+dependencies {
+    // JAX-RS annotations an interfaces, provided by Jersey at runtime
+    compileOnly("jakarta.ws.rs", "jakarta.ws.rs-api", "2.1.6")
+    val jerseyVersion = "2.30"
+    implementation("org.glassfish.jersey.containers", "jersey-container-jdk-http", jerseyVersion)
+    // lets us interop between JAX-RS and jackson beautifully
+    implementation("org.glassfish.jersey.media", "jersey-media-json-jackson", jerseyVersion)
+    // Eclipse Foundation lightweight dependency injection framework, used by Jersey internally
+    runtimeOnly("org.glassfish.jersey.inject", "jersey-hk2", jerseyVersion)
+    // JAXB runtime was removed from JRE 11, but is still used by the Jersey JAX-RS implementation
+    runtimeOnly("org.glassfish.jaxb", "jaxb-runtime", "2.3.2")
+    // AutoValue for reducing boilerplate in creating immutable value objects
+    val autoValueVersion = "1.7"
+    compileOnly("com.google.auto.value", "auto-value-annotations", autoValueVersion)
+    annotationProcessor("com.google.auto.value", "auto-value", autoValueVersion)
+}
