aedb1ce8475e41e14edc25ace9a5d8d698088e9e
Ryan Dens <ryan.dens@contrastsecurity.com>
2020-05-09 14:43:56 -0400

    :sparkles: register CoffeeService and jacksonJaxbProvider

--- provider/src/main/java/com/github/ryandens/provider/Main.java
+++ provider/src/main/java/com/github/ryandens/provider/Main.java
@@ -1,11 +1,41 @@
 package com.github.ryandens.provider;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Set;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Feature;
+import javax.ws.rs.core.UriBuilder;
+import org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
 public final class Main {
 
   /**
    * Entry point of the API Provider application, responsible for starting and stopping resources
    */
-  public static void main(final String[] args) {
-    System.out.println("hello world");
+  public static void main(final String[] args) throws InterruptedException {
+    final var services =
+        Set.of(
+            new CoffeeService(new PriceService(), new ObjectMapper()),
+            new JacksonJaxbJsonProvider());
+    final var resourceConfig =
+        ResourceConfig.forApplication(
+            new Application() {
+              @Override
+              public Set<Object> getSingletons() {
+                return Set.of(
+                    (Feature)
+                        context -> {
+                          services.forEach(context::register);
+                          return true;
+                        });
+              }
+            });
+    final var httpServer =
+        JdkHttpServerFactory.createHttpServer(
+            UriBuilder.fromUri("http://localhost/").port(8080).build(), resourceConfig, true);
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> httpServer.stop(0)));
+    Thread.currentThread().join();
   }
 }
